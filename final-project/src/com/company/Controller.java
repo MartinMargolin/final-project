f982e638e4882ea88983059710f466df0f95a0cf
Dustin Miller <dustinamiller15@gmail.com>
2021-03-03 11:02:16 -0700

    mainMenu();

--- final-project/src/com/company/Controller.java
+++ final-project/src/com/company/Controller.java
@@ -15,6 +15,60 @@ public class Controller
         new Board().printBoard("Yeuh", "Yeuh");
     }
 
+    public void mainMenu() {
+        System.out.println("\n\n");
+        String mainMenu = new StringBuilder("Main Menu\n\n").
+                append("1) Player vs. Player\n").
+                append("0) Exit\n\n").append("Enter the number of your selection: ").toString();
+        int minOption = 0;
+        int maxOption = 1;
+        int userChoice;
+
+
+        do {
+            userChoice = promptForInt(mainMenu, minOption, maxOption);
+            switch (userChoice) {
+                case 1:
+                    /*humanVHuman();*/
+                    /*prompt =  promptForInt("Would you like to play again? \n 1: Yes    0: No \n", 0,1);
+                    if(prompt == 0)
+                    {
+                        userChoice = 0;
+                        break;
+                    } else
+                    {
+                        break;
+                    }*/
+
+                case 2:
+                    /*humanVComp();
+                    prompt =  promptForInt("Would you like to play again? \n 1: Yes    0: No \n", 0,1);
+                    if(prompt == 0)
+                    {
+                        userChoice = 0;
+                        break;
+                    } else
+                    {
+                        break;
+                    }*/
+
+                case 3:
+                    /*compVComp();
+                    prompt =  promptForInt("Would you like to play again? \n 1: Yes    0: No \n", 0,1);
+                    if(prompt == 0)
+                    {
+                        userChoice = 0;
+                        break;
+                    } else
+                    {
+                        break;
+                    }*/
+
+
+            }
+        } while (userChoice > 0);
+    }
+
 
     private int promptForInt(String prompt, int min, int max) {
         if (min > max) {
