037772d3cdd1bbc57b29ddfa08b4c629b5e0bd22
mmargolin <mmargolin@student.neumont.edu>
2021-03-03 10:36:44 -0700

    Adding to Controller

--- final-project/src/com/company/Controller.java
+++ final-project/src/com/company/Controller.java
@@ -1,10 +1,46 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Controller
 {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
     public void run()
     {
 
+    }
+
+
+    private int promptForInt(String prompt, int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("The min cannot be greater than the max. min: " + min + ", max: " + max);
+        }
+
+        int userNum = -1;
+        String input = null;
+        boolean isInvalid = true;
+
+        do {
+            System.out.print(prompt);
+            try {
+                input = bufferedReader.readLine();
+                userNum = Integer.parseInt(input);
+                isInvalid = userNum < min || userNum > max;
+            } catch (IOException ioe) {
+                System.out.println("Whoa! Something technical went wrong. Let's try that again.");
+            } catch (NumberFormatException nfe) {
+
+            }
+
+            if (isInvalid) {
+                System.out.println("You entered an invalid value. Please, try again.");
+            }
+        } while (isInvalid);
+
+        return userNum;
     }
 
 }
