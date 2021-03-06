d2c9336ad1011a0924834d4307af0e2e572e68a1
Dustin Miller <dustinamiller15@gmail.com>
2021-03-06 12:44:22 -0700

    playerVPlayer

--- final-project/src/com/company/Controller.java
+++ final-project/src/com/company/Controller.java
@@ -9,10 +9,89 @@ import java.io.InputStreamReader;
 public class Controller
 {
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int turnRotation;
 
     public void run()
     {
         new Board().printBoard("Yeuh", "Yeuh");
+    }
+
+    public void playerVPlayer() {
+        String in;
+
+        System.out.println("Player one enter name ");
+
+        in = input();
+        /*if (!in.equals("")) {
+            P1.setName(in);
+        } else {
+            P1.setName("Human1");
+        }*/
+
+        System.out.println("Player two enter name ");
+
+        in = input();
+        /*if (!in.equals("")) {
+            P2.setName(in);
+        } else {
+            P2.setName("Human2");
+        }*/
+
+        Board board = new Board();
+        int game = 1;
+        /*board.printBoard(P1.getName(), P2.getName());
+        startingPlayer(P1.getName(), P2.getName());*/
+
+        /*if (turnRotation == 1) {
+            P1.setColor("Y");
+            P2.setColor("R");
+        } else if (turnRotation == 2) {
+            P2.setColor("Y");
+            P1.setColor("R");
+        }*/
+
+        do {
+
+            switch (turnRotation) {
+
+                case 1:
+                    /*board.printBoard(P1.getName(), P2.getName());
+                    turn = promptForInt("\n\n" + P1.getName() + " place your piece (1-7): ", 1, 7);
+                    try {
+                        board.placePiece(P1.getColor(), turn);
+                    } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+                        System.out.println("Your move was invalid. " + "\n" + "Try again!!");
+                    }
+
+                    if (board.checkWinner(P1.getColor())) {
+                        System.out.println("Player:" + P1.getName() + " has won the game!");
+                        game = 0;
+                        break;
+                    }
+
+                    turnRotation = 2;
+                    break;*/
+
+                case 2:
+                    /*board.printBoard(P1.getName(), P2.getName());
+                    turn = promptForInt("\n\n" + P2.getName() + " place your piece (1-7): ", 1, 7);
+
+                    try {
+                        board.placePiece(P2.getColor(), turn);
+                    } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+                        System.out.println("Your move was invalid. " + "\n" + "Try again!!");
+                    }
+
+                    if (board.checkWinner(P2.getColor())) {
+                        System.out.println("Player:" + P2.getName() + " has won the game!");
+                        game = 0;
+                        break;
+                    }*/
+
+                    turnRotation = 1;
+                    break;
+            }
+        } while (game == 1);
     }
 
     public void mainMenu() {
@@ -71,6 +150,18 @@ public class Controller
         } while (isInvalid);
 
         return userNum;
+    }
+
+    public String input() {
+        System.out.print("Input: ");
+        String input = "";
+        try {
+            input = bufferedReader.readLine();
+        } catch (Exception e) {
+            System.out.println("Slow down there pal lets try that again...");
+        }
+        System.out.println(input);
+        return input;
     }
 
 }
